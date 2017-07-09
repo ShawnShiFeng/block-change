098df8b4f56b695795aae7c29a2016171f465daf
Shawn Feng <shawnsfeng@gmail.com>
2017-07-09 12:33:26 -0700

    style change

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -7,7 +7,7 @@ const Transaction = (props) => (
     <div style={{ padding: "5px"}}>Block Height: {props.transaction.blockNumber}</div>
     <div style={{ padding: "5px"}}>From: {props.transaction.from}</div>
     <div style={{ padding: "5px"}}>To: {props.transaction.to}</div>
-    <div style={{ padding: "5px"}}>Value: {props.transaction.value}</div>
+    <div style={{ padding: "5px"}}>Value: {props.transaction.value/1000000000000000000}</div>
     <Divider />
   </div>
 );
